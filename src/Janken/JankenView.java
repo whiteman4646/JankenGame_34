69d498d1deea6f27425fe81aaba7814c8891a29e
hayato maeda <h.maeda1711@morijyobi.ac.jp>
2018-08-24 12:14:50 +0900

    JankenView.java じゃんけんボタン制御の実装
     guButton.setOnAction, cyokiButton.setOnAction, paButton.setOnActionを作成した

--- src/Janken/JankenView.java
+++ src/Janken/JankenView.java
@@ -21,6 +21,7 @@ public class JankenView extends Application {
 	private Button guButton;
 	private Button cyokiButton;
 	private Button paButton;
+	private JankenController jc;//じゃんけん制御オブジェクト変数
 
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -31,6 +32,13 @@ public class JankenView extends Application {
 		myLayout(stage);
 
 		stage.show();
+
+		//じゃんけん制御クラスのインスタンス
+        jc = new JankenController(cpuLabel, humanLabel, resultLabel,scoreLabel);
+        //イベント処理
+        guButton.setOnAction(event -> jc.putOutRockHand());
+        cyokiButton.setOnAction(evet -> jc.putOutScissorsHand());
+        paButton.setOnAction(evet -> jc.putOutPaperHand());
 	}
 
 	private void myLayout(Stage stage) {
